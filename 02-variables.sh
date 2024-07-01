8df7226bd3876b8139e6032a526e8760cb41a827
Paul Prem Kumar <paul.prem5@gmail.com>
2024-07-02 04:23:18 +0530

    initial push to central repo

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,6 +4,8 @@ PERSON1=Saroja
 PERSON2=Karpuram
 
 echo "${PERSON1}:: Hello ${PERSON2} How are you ?"
-echo "${PERSON2}::  I'm doing good ${PERSON1}" 
+echo "${PERSON2}:: I'm doing good ${PERSON1}" 
+echo "${PERSON1}:: Eroju Collection Enta"
+echo "${PERSON2}:: Eroju antaa Goppaga m ledu ${PERSON1}"
 
 echo "${PERSON1} ${PERSON2} both are fail"
