f3f8ca8178db867334402b6000fa7ef54c42d106
Paul Prem Kumar <paul.prem5@gmail.com>
2024-07-02 04:33:21 +0530

    initial push to central repo

---
+++ 03-arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "${PERSON1}:: Hello ${PERSON2} How are you ?"
+echo "${PERSON2}:: I'm doing good ${PERSON1}" 
+echo "${PERSON1}:: Eroju Collection Enta?"
+echo "${PERSON2}:: Eroju antaa Goppaga m ledu ${PERSON1}"
+echo "so ${PERSON1} & ${PERSON2} both are cotton business magnets"
+echo "${PERSON1} ${PERSON2} both are fail"
