7e330970331537454410ebf8c61d44237eb59be1
Paul Prem Kumar <paul.prem5@gmail.com>
2024-07-02 04:26:06 +0530

    initial push to central repo

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,7 +5,7 @@ PERSON2=Karpuram
 
 echo "${PERSON1}:: Hello ${PERSON2} How are you ?"
 echo "${PERSON2}:: I'm doing good ${PERSON1}" 
-echo "${PERSON1}:: Eroju Collection Enta"
+echo "${PERSON1}:: Eroju Collection Enta?"
 echo "${PERSON2}:: Eroju antaa Goppaga m ledu ${PERSON1}"
-
+echo "so ${PERSON1} & ${PERSON2} both are cotton business magnets"
 echo "${PERSON1} ${PERSON2} both are fail"
